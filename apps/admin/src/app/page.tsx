dd7f6063efe2b2e7c3f22b5e7f5498fcbc760e90
alotofhee <ekgmljeong@gmail.com>
2025-07-10 11:11:34 +0900

    chore: app, componets 디렉토리 src 내로 이동

---
+++ apps/admin/src/app/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@workspace/ui/components/button";
+import { Progress } from "@workspace/ui/components/progress";
+
+export default function Page() {
+  return <div>
+    <Button variant="default">다음</Button>
+    <Button variant="disabled">다음</Button>
+    <Button variant="outline">다음</Button>
+    <Button variant="onb_select">다음</Button>
+    <Button variant="onb_selected">다음</Button>
+    <Button variant="unselected">다음</Button>
+    <Button variant="period_selected">다음</Button>
+    <Button variant="kakao">다음</Button>
+    <Button variant="black">다음</Button>
+    <div></div>
+  </div>;
+}
