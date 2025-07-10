1cb3041b58f130eda33746c11093d7b4d63ce676
sujinRo <shtnwls1111@ewhain.net>
2025-07-10 15:16:53 +0900

    UBLE-21 feat: 전역 스타일링(globals.css) 설정

---
+++ apps/admin/src/app/layout.tsx
@@ -0,0 +1,16 @@
+import "../../../packages/ui/src/styles/globals.css";
+import { Providers } from "@/src/components/providers";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
