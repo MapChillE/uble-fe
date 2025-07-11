897330f1dc52ea26d5cc324bc5cad35eeb6ec4d7
alotofhee <ekgmljeong@gmail.com>
2025-07-11 10:28:37 +0900

    UBLE-20 feat: shadcn progress 색상 수정

--- packages/ui/src/components/progress.tsx
+++ packages/ui/src/components/progress.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as ProgressPrimitive from "@radix-ui/react-progress"
+import * as React from "react";
+import * as ProgressPrimitive from "@radix-ui/react-progress";
 
-import { cn } from "@workspace/ui/lib/utils"
+import { cn } from "@workspace/ui/lib/utils";
 
 function Progress({
   className,
@@ -13,19 +13,16 @@ function Progress({
   return (
     <ProgressPrimitive.Root
       data-slot="progress"
-      className={cn(
-        "relative h-2 w-full overflow-hidden rounded-full bg-background-secondary",
-        className
-      )}
+      className={cn("relative h-2 w-full overflow-hidden rounded-full bg-gray-200", className)}
       {...props}
     >
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
-        className="h-full w-full flex-1 bg-spot-green transition-all"
+        className="h-full w-full flex-1 bg-green-500 transition-all"
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
     </ProgressPrimitive.Root>
-  )
+  );
 }
 
-export { Progress }
+export { Progress };
