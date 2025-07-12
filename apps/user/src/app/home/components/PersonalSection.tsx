b259b944cefe970dd46ad95a3609980fe38c4b3d
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-07-12 14:01:31 +0900

    UBLE-22 comment: 불필요한 주석 제거

--- apps/user/src/app/home/components/PersonalSection.tsx
+++ apps/user/src/app/home/components/PersonalSection.tsx
@@ -3,8 +3,6 @@ import SectionHeader from '../../../components/ui/SectionHeader';
 import DynamicCard from '../../../components/ui/DynamicCard';
 
 const PersonalSection = () => {
-  // section 하나당 하나의 api 응답을 가짐
-  // props로 요청주소를 보내면 그거에 해당하는 카드 리스트 출력하게?
   return (
     <div className="space-y-4">
       <SectionHeader title='개인별 추천' />
