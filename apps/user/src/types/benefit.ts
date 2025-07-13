bfb3fda2c72f48b424db4362e64cf2f83bab72c1
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-07-13 17:22:49 +0900

    UBLE-31 test: 혜택 이용 내역 테스트 데이터 추가

---
+++ apps/user/src/types/benefit.ts
@@ -0,0 +1,9 @@
+export interface Benefit {
+  bookmarkId: number;
+  brandId: number;
+  brandName: string;
+  storeName: string;
+  category: string;
+  brandImageUrl: string;
+  usedAt: string;
+}
