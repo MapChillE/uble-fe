fdcbe14458f4f8603c368438b8ecd6aa72d6a0ea
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-07-14 11:22:40 +0900

    UBLE-35 feat: 제휴처 페이지 UI 마크업, 책임 분리

--- apps/user/src/app/(main)/home/page.tsx
+++ apps/user/src/app/(main)/home/page.tsx
@@ -3,10 +3,12 @@ import PersonalSection from "@/app/(main)/home/components/PersonalSection";
 import TimeSection from "@/app/(main)/home/components/TimeSection";
 import React from "react";
 import EntireSection from "./components/EntireSection";
+import SearchSection from "./components/SearchSection";
 
 const page = () => {
   return (
     <section className="space-y-8">
+      <SearchSection />
       <PersonalSection />
       <AgeSection />
       <TimeSection />
