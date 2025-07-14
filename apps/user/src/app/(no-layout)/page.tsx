e2d97097ac76586bccf8ff5211f656dd16b9a732
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-07-14 14:02:48 +0900

    UBLE-37 feat: 불필요한 아이콘 삭제

--- apps/user/src/app/(no-layout)/page.tsx
+++ apps/user/src/app/(no-layout)/page.tsx
@@ -4,11 +4,8 @@ import KakaoLoginBtn from "./components/KakaoLoginBtn";
 export default function Page() {
   return (
     <div>
-      <div className="px-4 py-6">
+      <div className="px-4 py-6 mt-15">
         <div className="text-center space-y-4">
-          <div className="w-16 h-16 bg-spot-green rounded-2xl flex items-center justify-center mx-auto">
-            <Zap className="w-8 h-8 text-white" />
-          </div>
           <div className="space-y-2">
             <h1 className="text-2xl font-bold text-text-primary">UBLE</h1>
           </div>
