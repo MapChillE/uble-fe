fb1223159b8c8a8eeb26aac947685c8f40bf6cde
alotofhee <ekgmljeong@gmail.com>
2025-07-14 09:41:45 +0900

    UBLE-27 feat: 즐겨찾기 버튼을 위한 variant 추가 및 size 수정

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -34,12 +34,15 @@ const buttonVariants = cva(
         modal_cancel:
           "bg-transparent border border-gray-300 bg-white shadow-xs hover:bg-gray-50 hover:text-gray-900",
         modal_submit: "bg-[#41d596] hover:bg-[#3bc085] text-white disabled:bg-gray-300",
+
+        circle_outline:
+          "rounded-full border border-gray-300 bg-white shadow-lg hover:bg-gray-50 hover:text-gray-900",
       },
       size: {
         default: "h-10 px-4 py-2 has-[>svg]:px-3",
         sm: "h-9 rounded-md px-3 gap-1.5 has-[>svg]:px-2.5",
         lg: "h-11 rounded-md px-8 has-[>svg]:px-4",
-        icon: "size-9",
+        icon: "size-9 h-10 w-10 p-0",
         full: "px-4 py-2 w-full h-12",
         select: "py-3 px-4",
         feedback: "flex-1",
