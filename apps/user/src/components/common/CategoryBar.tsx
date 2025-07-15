c6af5e8a1b0d14be22f7c6bc3a6861327fefe383
alotofhee <ekgmljeong@gmail.com>
2025-07-15 21:09:11 +0900

    UBLE-27  refactor: CategoryBar zustand 전역 상태로 카테고리 관리하도록 변경

--- apps/user/src/components/common/CategoryBar.tsx
+++ apps/user/src/components/common/CategoryBar.tsx
@@ -1,22 +1,21 @@
 "use client";
 
 import { Button } from "@workspace/ui/components/button";
-import { CATEGORIES, Category } from "@/types/constants";
+import { CATEGORIES } from "@/types/constants";
+import { useMapStore } from "@/store/useMapStore";
 
-type CategoryBarProps = {
-  selected: Category;
-  onSelect: (category: Category) => void;
-};
+export default function CategoryBar() {
+  const { selectedCategory, setSelectedCategory } = useMapStore();
 
-export default function CategoryBar({ selected, onSelect }: CategoryBarProps) {
   return (
     <nav className="scrollbar-hide absolute left-4 right-4 top-16 z-10 flex gap-2 overflow-x-auto whitespace-nowrap py-3">
+      {/* TODO:  추후 백엔드에서 받아온 CATEGORIES로 사용 */}
       {CATEGORIES.map((cat) => (
         <Button
           key={cat}
-          variant={selected === cat ? "filter_select" : "filter_unselect"}
+          variant={selectedCategory === cat ? "filter_select" : "filter_unselect"}
           size="sm"
-          onClick={() => onSelect(cat)}
+          onClick={() => setSelectedCategory(cat)}
         >
           {cat}
         </Button>
