a0ca1509e3b06b080070121e31f0a7424f57bc7d
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-07-16 22:29:30 +0900

    UBLE-44 feat: tanstack query 사용 컴포넌트 provider 구현

--- apps/user/src/app/(main)/home/page.tsx
+++ apps/user/src/app/(main)/home/page.tsx
@@ -1,9 +1,8 @@
 import AgeSection from "@/app/(main)/home/components/AgeSection";
 import PersonalSection from "@/app/(main)/home/components/PersonalSection";
 import TimeSection from "@/app/(main)/home/components/TimeSection";
-
-import EntireSection from "./components/EntireSection";
 import SearchSection from "./components/SearchSection";
+import EntireSectionProvider from "./components/EntireSectionProvider";
 
 const page = () => {
   return (
@@ -12,7 +11,7 @@ const page = () => {
       <PersonalSection />
       <AgeSection />
       <TimeSection />
-      <EntireSection />
+      <EntireSectionProvider />
     </section>
   );
 };
