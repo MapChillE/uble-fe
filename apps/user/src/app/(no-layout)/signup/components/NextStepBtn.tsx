73b860624c6a3c90afb70dffa00a80672ab64c61
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-07-16 09:45:31 +0900

    UBLE-39 feat: 다음 단계 버튼 구현

---
+++ apps/user/src/app/(no-layout)/signup/components/NextStepBtn.tsx
@@ -0,0 +1,43 @@
+'use client'
+import { Button } from '@workspace/ui/components/button';
+import { useRouter } from 'next/navigation';
+import { Dispatch, SetStateAction } from 'react';
+
+interface NextStepBtnProps {
+  currentStep: number;
+  canProceed: () => boolean;
+  setCurrentStep: Dispatch<SetStateAction<number>>;
+}
+const NextStepBtn = ({ currentStep, canProceed, setCurrentStep }: NextStepBtnProps) => {
+  const router = useRouter();
+  const handleNext = () => {
+    if (currentStep < 5) {
+      setCurrentStep(currentStep + 1)
+    } else {
+      // completeOnboarding({
+      //   grade: formData.grade,
+      //   birthDate: formData.birthDate,
+      //   gender: formData.gender,
+      //   preferences: { interests: formData.interests },
+      //   barcodeNumber: formData.barcodeNumber,
+      // })
+      // 여기서 비동기 처리 후 routing
+      router.push("/home")
+    }
+  }
+
+  return (
+    <div className="p-5 pt-0">
+      <Button
+        onClick={handleNext}
+        disabled={!canProceed()}
+        className={`w-full h-12 text-sm font-semibold rounded-lg transition-all ${canProceed() ? "bg-[#41d596] hover:bg-[#14B470] text-white" : "bg-gray-200 text-gray-400 cursor-not-allowed"
+          }`}
+      >
+        {currentStep === 5 ? "시작하기" : "다음"}
+      </Button>
+    </div>
+  );
+};
+
+export default NextStepBtn;
