3f2e0c2a93d2658020b83fe32dbbda82e985cb10
alotofhee <ekgmljeong@gmail.com>
2025-07-17 16:25:00 +0900

    UBLE-43 feat: 에러처리 추가 및 handleFetchCategories 함수 추가

--- apps/user/src/app/(main)/map/components/MapContainer.tsx
+++ apps/user/src/app/(main)/map/components/MapContainer.tsx
@@ -3,13 +3,14 @@
 import { Star } from "lucide-react";
 import { Button } from "@workspace/ui/components/button";
 import SearchSection from "@/components/common/SearchSection";
-import MapWithBaseLocation from "@/app/(main)/map/components/MapWithBaseLocation";
+import CurrentLocationMap from "@/app/(main)/map/components/MapWithBaseLocation";
 import MyPlaceSheet from "@/app/(main)/map/components/MyPlaceSheet";
-import CategorySection from "@/app/(main)/map/components/CategorySection";
+import CategorySection from "@/components/common/CategorySection";
 import { useEffect, useMemo, useState } from "react";
 import { Category } from "@/types/category";
 import { getCategories } from "@/service/category";
 import { useCategoryStore } from "@/store/useCategoryStore";
+import { apiHandler } from "@api/apiHandler";
 
 const ALL_CATEGORY: Category = { categoryId: 0, categoryName: "전체" };
 
@@ -18,25 +19,27 @@ export default function MapContainer() {
   const setCategories = useCategoryStore((s) => s.setCategories);
   // const categories = useCategoryStore((s) => s.categories);
   // TODO: useCategoryStore에서 카테고리 존재 여부 if문 확인해서 추가 or api + 추가
+  const handleFetchCategories = async () => {
+    const { data, error } = await apiHandler(() => getCategories());
+    if (error) {
+      console.log("카테고리 로딩 실패: ", error);
+      // TODO: 에러 상태 관리 또는 토스트 알림 추가
+    }
+    if (data && data.categoryList) {
+      setCategories([
+        ALL_CATEGORY,
+        ...data.categoryList.map((category) => ({
+          categoryId: category.categoryId,
+          categoryName: category.categoryName,
+        })),
+      ]);
+    }
+  };
+
   useEffect(() => {
-    getCategories().then(({ data, error }) => {
-      if (error) {
-        console.log("카테고리 로딩 실패: ", error);
-        // 에러 상태 관리 또는 토스트 알림 추가
-        return;
-      }
-      if (data && data.data && Array.isArray(data.data.data.categoryList)) {
-        setCategories([
-          ALL_CATEGORY,
-          ...data.data.data.categoryList.map((cat) => ({
-            categoryId: cat.categoryId,
-            categoryName: cat.categoryName,
-          })),
-        ]);
-      }
-      setSelectedCategory({ ...ALL_CATEGORY });
-    });
-  }, [setCategories]);
+    handleFetchCategories();
+    setSelectedCategory({ ...ALL_CATEGORY });
+  }, []);
 
   const trigger = useMemo(
     () => (
@@ -49,7 +52,7 @@ export default function MapContainer() {
   return (
     <div className="relative h-screen w-full">
       {/* {categories.length > 0 && ( */}
-      <MapWithBaseLocation zoom={15} selectedCategory={selectedCategory} />
+      <CurrentLocationMap zoom={15} selectedCategory={selectedCategory} />
       {/* )} */}
       <div className="absolute left-0 right-0 top-0 z-10">
         <SearchSection />
