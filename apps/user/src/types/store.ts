667b3bf87963f7f459e41404078803d92aed426b
alotofhee <ekgmljeong@gmail.com>
2025-07-18 16:23:10 +0900

    UBLE-47 feat: 매장 상세 정보 타입 추가

--- apps/user/src/types/store.ts
+++ apps/user/src/types/store.ts
@@ -30,3 +30,57 @@ export interface StoreListResponse extends responseStatus {
     };
   };
 }
+
+/**
+ * 매장(스토어) 간략 정보 타입 (API 미구현으로 임시)
+ */
+export interface StoreSummary {
+  // TODO: 상세 API 요청시 storeId로 보낼 수 있으니까 필요 여부 확인.
+  // brandId: number;
+  // storeId: number;
+  storeName: string;
+  description: string;
+  address: string;
+  phoneNumber: string;
+  distance: number;
+  category: string;
+}
+
+/**
+ * 매장(스토어) 혜택 타입
+ */
+export interface StoreBenefit {
+  benefitId: number;
+  type: string;
+  minRank: string;
+  content: string;
+  manual: string;
+  provisionCount: string;
+}
+
+/**
+ * 매장(스토어) 상세 API 응답 타입
+ */
+export interface StoreDetail {
+  brandId: number;
+  storeId: number;
+  storeName: string;
+  description: string;
+  address: string;
+  phoneNumber: string;
+  distance: number;
+  category: string;
+  imageUrl?: string;
+  benefitList: StoreBenefit[];
+  normalAvailable: boolean;
+  vipAvailable: boolean;
+  localAvailable: boolean;
+}
+
+/**
+ * 매장 상세 정보 API
+ */ export interface StoreDetailResponse {
+  data: StoreDetail;
+  statusCode: number;
+  message?: string;
+}
