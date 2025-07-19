f71d8ca9ac6e9ae3b9e078b68c58eb8b4647d925
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-07-19 20:45:34 +0900

    UBLE-51 feat: 사용자가 서비스에 접속 시 사용자 정보를 layout 에서 불러옴

--- apps/user/src/app/(main)/layout.tsx
+++ apps/user/src/app/(main)/layout.tsx
@@ -3,6 +3,7 @@ import Footer from "../../components/common/Footer";
 import Header from "../../components/common/Header";
 
 import { Providers } from "@/components/providers";
+import HydrateData from "@/components/HydrateData";
 
 export default function RootLayout({
   children,
@@ -13,6 +14,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body>
         <Providers>
+          <HydrateData />
           <Header />
           <main style={{ paddingBottom: "72px" }}>{children}</main>
           <Footer />
