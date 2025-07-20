834858bef2e5d00a06de46a3f82c3178ddfb9870
alotofhee <ekgmljeong@gmail.com>
2025-07-20 18:30:10 +0900

    UBLE-50 chore: 외부 이미지 도메인 추가(aws s3)

--- apps/user/next.config.mjs
+++ apps/user/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "uble-storage.s3.ap-northeast-2.amazonaws.com",
+      },
+    ],
+  },
+};
 
-export default nextConfig
+export default nextConfig;
