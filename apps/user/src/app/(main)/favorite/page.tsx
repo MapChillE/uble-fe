2fe273f9b200c37121d57325a41e7df0b4eb1fdb
alotofhee <ekgmljeong@gmail.com>
2025-07-21 16:48:27 +0900

    UBLE-56 feat: 즐겨찾기 페이지 생성

---
+++ apps/user/src/app/(main)/favorite/page.tsx
@@ -0,0 +1,13 @@
+import FavoriteSectionProvider from "@/app/(main)/favorite/components/FavoriteSectionProvider";
+
+export default function FavoritePage() {
+  return (
+    <section>
+      {/* <hr className="border-gray-300" /> */}
+      <div className="space-y-4 p-4">
+        <h3 className="text-lg font-semibold">즐겨찾기</h3>
+        {/* <FavoriteSectionProvider /> */}
+      </div>
+    </section>
+  );
+}
