1bea6804068d77861751d40abd6b78d6a04cffcd
alotofhee <ekgmljeong@gmail.com>
2025-07-23 01:16:06 +0900

    UBLE-60 feat: QueryClientProvider 전역으로 적용

--- apps/user/src/components/providers.tsx
+++ apps/user/src/components/providers.tsx
@@ -2,6 +2,8 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/queryClient";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
@@ -12,7 +14,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
       disableTransitionOnChange
       enableColorScheme
     >
-      {children}
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </NextThemesProvider>
   );
 }
