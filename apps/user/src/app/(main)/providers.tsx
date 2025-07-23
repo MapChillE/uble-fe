161c6c70d1cd4f8e46a98645a0bbc5e8307ffec1
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-07-23 09:19:35 +0900

    UBLE-59 refactor: queryClient를 state로 관리

--- apps/user/src/app/(main)/providers.tsx
+++ apps/user/src/app/(main)/providers.tsx
@@ -1,10 +1,19 @@
 'use client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
 
 const queryClient = new QueryClient();
 
 export function ReactQueryProvider({ children }: React.PropsWithChildren) {
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 5,
+        refetchOnWindowFocus: false,
+      },
+    },
+  }));
   return (
     <QueryClientProvider client={queryClient}>
       {children}
