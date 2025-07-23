ee12d54e86b2752468545d59b77c99f558d20915
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-07-24 00:35:18 +0900

    UBLE-71 feat: 사용자 중복 요청 방지 로직 추가

--- apps/user/src/components/modal/BenefitConfirmModal.tsx
+++ apps/user/src/components/modal/BenefitConfirmModal.tsx
@@ -6,10 +6,11 @@ import { UsageRegistReq } from "@/types/usage";
 import { apiHandler } from "@api/apiHandler";
 import { setUsage } from "@/service/usage";
 import { toast } from "sonner";
+import { useState } from "react";
 
 const BenefitConfirmModal = () => {
   const { isOpen, close, storeId, isVIPcock, resetInfo } = useBenefitConfirmModalStore();
-
+  const [isLoading, setIsLoading] = useState(false);
   const handleClose = () => {
     close();
     resetInfo();
@@ -17,9 +18,10 @@ const BenefitConfirmModal = () => {
 
   const handleYes = async (benefitType: "NORMAL" | "VIP" = "NORMAL") => {
     if (storeId !== null) {
+      setIsLoading(true);
       const params: UsageRegistReq = { benefitType };
       const { data } = await apiHandler(() => setUsage(storeId, params));
-      console.log(data);
+      setIsLoading(false);
       if (data?.statusCode === 0) {
         toast.info("사용 완료 되었습니다.");
       } else if (data?.statusCode === 2001) {
@@ -28,7 +30,7 @@ const BenefitConfirmModal = () => {
         toast.error("사용 중 오류가 발생했습니다.");
       }
     } else {
-      alert("혜택 사용 실패");
+      toast.error("네트워크 오류가 발생했습니다.");
     }
     handleClose();
     // 필요시 추가 로직
@@ -46,11 +48,21 @@ const BenefitConfirmModal = () => {
           <Button type="button" variant="modal_cancel" onClick={handleClose}>
             아니요
           </Button>
-          <Button type="button" onClick={() => handleYes("NORMAL")} variant="modal_submit">
+          <Button
+            type="button"
+            onClick={() => handleYes("NORMAL")}
+            variant="modal_submit"
+            disabled={isLoading}
+          >
             일반 혜택
           </Button>
           {isVIPcock && (
-            <Button type="button" onClick={() => handleYes("VIP")} variant="modal_submit">
+            <Button
+              type="button"
+              onClick={() => handleYes("VIP")}
+              variant="modal_submit"
+              disabled={isLoading}
+            >
               VIP콕 혜택
             </Button>
           )}
