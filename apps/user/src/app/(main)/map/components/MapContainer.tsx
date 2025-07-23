9b162ddf44492096bccb5ec8ffbc7df66ca5b72e
alotofhee <ekgmljeong@gmail.com>
2025-07-23 14:46:55 +0900

    UBLE-70 refactor: MapContainer에서 categories가 store에 있는지 여부에 따라 분기처리

--- apps/user/src/app/(main)/map/components/MapContainer.tsx
+++ apps/user/src/app/(main)/map/components/MapContainer.tsx
@@ -26,36 +26,37 @@ import { Pin } from "@/app/(main)/map/components/NaverMap";
 
 export default function MapContainer() {
   const [selectedCategory, setSelectedCategory] = useState<Category>(ALL_CATEGORY);
+  const categories = useCategoryStore((s) => s.categories);
   const setCategories = useCategoryStore((s) => s.setCategories);
-  // const categories = useCategoryStore((s) => s.categories);
+  const setUserCategories = useCategoryStore((s) => s.setUserCategories);
 
   const [isOpen, setIsOpen] = useState(false);
   const [snapIndex, setSnapIndex] = useState(1);
   const [storeDetail, setStoreDetail] = useState<StoreDetail | null>(null);
 
-  // TODO: useCategoryStore에서 카테고리 존재 여부 if문 확인해서 추가 or api + 추가
-  const handleFetchCategories = async () => {
-    const { data, error } = await apiHandler(() => getCategories());
-    if (error) {
-      console.log("카테고리 로딩 실패: ", error);
-      // TODO: 에러 상태 관리 또는 토스트 알림 추가
-    }
-    if (data && data.categoryList) {
-      setCategories([
-        ALL_CATEGORY,
-        ...data.categoryList.map((category) => ({
-          categoryId: category.categoryId,
-          categoryName: category.categoryName,
-        })),
-        ...ANY_CATEGORYS,
-      ]);
-    }
-  };
-
   useEffect(() => {
-    handleFetchCategories();
-    setSelectedCategory({ ...ALL_CATEGORY });
-  }, []);
+    if (categories.length === 0) {
+      const handleFetchCategories = async () => {
+        const { data, error } = await apiHandler(() => getCategories());
+        if (error) {
+          console.log("카테고리 로딩 실패: ", error);
+          // TODO: 에러 상태 관리 또는 토스트 알림 추가
+        }
+        if (data?.categoryList) {
+          setCategories([
+            ALL_CATEGORY,
+            ...data.categoryList.map((category) => ({
+              ...category,
+            })),
+            ...ANY_CATEGORYS,
+          ]);
+          setUserCategories(data.categoryList);
+        }
+      };
+
+      handleFetchCategories();
+    }
+  }, [categories, setCategories]);
 
   const { location: currentLocation, getCurrentLocation } = useCurrentLocation();
   useEffect(() => {
