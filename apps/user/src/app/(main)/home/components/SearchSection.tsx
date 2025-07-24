9217ced65e67d3da9d986e56ecb1de64f21d0a20
alotofhee <ekgmljeong@gmail.com>
2025-07-25 03:16:53 +0900

    UBLE-87 feat: 검색 결과 컴포넌트 생성 및 홈 콘텐츠 컴포넌트 분리

--- apps/user/src/app/(main)/home/components/SearchSection.tsx
+++ apps/user/src/app/(main)/home/components/SearchSection.tsx
@@ -1,9 +1,9 @@
-'use client'
-import SearchInput from '@/components/common/SearchInput';
-import { useState } from 'react';
+"use client";
+import SearchInput from "@/components/common/SearchInput";
+import { useState } from "react";
 
 const SearchSection = () => {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(e.target.value);
@@ -11,10 +11,7 @@ const SearchSection = () => {
 
   return (
     <section className="p-4">
-      <SearchInput
-        searchQuery={searchQuery}
-        handleChange={handleChange}
-      />
+      <SearchInput searchQuery={searchQuery} handleChange={handleChange} />
     </section>
   );
 };
