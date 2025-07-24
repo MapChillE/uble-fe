203ca4134eef4ce16100bc29a382612e537a9742
alotofhee <ekgmljeong@gmail.com>
2025-07-25 03:17:38 +0900

    UBLE-87 style: SearchSection 상단에 고정 및 page 컴포넌트 정리

--- apps/user/src/app/(main)/home/page.tsx
+++ apps/user/src/app/(main)/home/page.tsx
@@ -1,20 +1,17 @@
-import AgeSection from "@/app/(main)/home/components/AgeSection";
-import PersonalSection from "@/app/(main)/home/components/PersonalSection";
-import TimeSection from "@/app/(main)/home/components/TimeSection";
 import SearchSection from "@/app/(main)/home/components/SearchSection";
-import EntireSection from "@/app/(main)/home/components/EntireSection";
+import SearchResults from "@/app/(main)/home/components/SearchResults";
+import HomeContent from "@/app/(main)/home/components/HomeContent";
 
-
-const page = () => {
+const HomePage = () => {
   return (
-    <section className="space-y-8">
-      <SearchSection />
-      <PersonalSection />
-      <AgeSection />
-      <TimeSection />
-      <EntireSection />
+    <section>
+      <div className="sticky top-12 z-10 bg-white">
+        <SearchSection />
+      </div>
+      <SearchResults />
+      <HomeContent />
     </section>
   );
 };
 
-export default page;
+export default HomePage;
