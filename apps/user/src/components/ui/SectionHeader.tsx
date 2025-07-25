ba3a26c54706469a11926bf74366bb034462d637
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-07-25 16:05:27 +0900

    UBLE-89 feat: SectionHeader에서 스크롤이 필요한 요소의 ref를 props로 받음

--- apps/user/src/components/ui/SectionHeader.tsx
+++ apps/user/src/components/ui/SectionHeader.tsx
@@ -1,27 +1,32 @@
+import { scroll } from "@/utils/scroll";
 import { Button } from "@workspace/ui/components/button";
 import { ChevronLeft, ChevronRight } from "lucide-react";
+import { RefObject } from "react";
 
 interface SectionHeaderProps {
   title: string;
   isScroll?: boolean;
+  ref?: RefObject<HTMLDivElement | null>;
 }
-const SectionHeader = ({ title, isScroll = true }: SectionHeaderProps) => {
+const SectionHeader = ({ title, isScroll = false, ref }: SectionHeaderProps) => {
   return (
     <div className="flex items-center justify-between px-4">
       <h2 className="text-lg font-semibold text-gray-900">{title}</h2>
-      {isScroll && (
+      {isScroll && ref && (
         <div className="flex gap-1">
           <Button
             variant="ghost"
             size="sm"
-            className="h-8 w-8 p-0" /*onClick={() => scroll("left")}*/
+            className="h-8 w-8 p-0"
+            onClick={() => scroll("left", ref)}
           >
             <ChevronLeft className="h-4 w-4" />
           </Button>
           <Button
             variant="ghost"
             size="sm"
-            className="h-8 w-8 p-0" /*onClick={() => scroll("right")}*/
+            className="h-8 w-8 p-0"
+            onClick={() => scroll("right", ref)}
           >
             <ChevronRight className="h-4 w-4" />
           </Button>
