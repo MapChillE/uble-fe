1a3175d4e6794a0fe81df882f279e4b642c5b01f
alotofhee <ekgmljeong@gmail.com>
2025-07-25 17:07:16 +0900

    UBLE-87 fix: home 페이지 suspense로 감쌈

--- apps/user/src/app/(main)/home/page.tsx
+++ apps/user/src/app/(main)/home/page.tsx
@@ -1,16 +1,19 @@
 import SearchSection from "@/app/(main)/home/components/SearchSection";
 import SearchResults from "@/app/(main)/home/components/SearchResults";
 import HomeContent from "@/app/(main)/home/components/HomeContent";
+import { Suspense } from "react";
 
 const HomePage = () => {
   return (
-    <section>
-      <div className="sticky top-12 z-10 bg-white">
-        <SearchSection />
-      </div>
-      <SearchResults />
-      <HomeContent />
-    </section>
+    <Suspense>
+      <section>
+        <div className="sticky top-12 z-10 bg-white">
+          <SearchSection />
+          <SearchResults />
+        </div>
+        <HomeContent />
+      </section>
+    </Suspense>
   );
 };
 
