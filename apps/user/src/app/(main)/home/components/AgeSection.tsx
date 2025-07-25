94c3d91946191b91641af41b7c794d6eb050ddc3
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-07-25 14:37:20 +0900

    UBLE-89 feat: 나이대별 추천 제휴처 출력

--- apps/user/src/app/(main)/home/components/AgeSection.tsx
+++ apps/user/src/app/(main)/home/components/AgeSection.tsx
@@ -1,6 +1,9 @@
+"use client";
 import SectionHeader from "@/components/ui/SectionHeader";
 import DynamicCard from "@/components/ui/DynamicCard";
 import { BrandContent } from "@/types/brand";
+import { useQuery } from "@tanstack/react-query";
+import { fetchAgeRecommend } from "@/service/brand";
 
 const AgeSection = () => {
   const testData: BrandContent = {
@@ -13,14 +16,23 @@ const AgeSection = () => {
     minRank: "NONE",
     isBookmarked: false,
   };
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["ageRecommend"],
+    queryFn: fetchAgeRecommend,
+  });
+  if (isLoading) return <div>로딩 중...</div>;
+  if (isError || !data) return <div>에러</div>;
+  const { ageRange, gender, recommendationsList } = data.data;
   return (
     <div className="space-y-4">
-      <SectionHeader title="20대같은 나이대별 추천" />
+      <SectionHeader title={`${ageRange}대 ${gender === "MALE" ? "남성" : "여성"} 추천 제휴처`} />
       <div
         className="scrollbar-hide flex gap-4 overflow-x-auto px-4 pb-2"
         style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
       >
-        <DynamicCard data={testData} />
+        {recommendationsList.map((item) => (
+          <DynamicCard data={item} key={item.brandId} />
+        ))}
       </div>
     </div>
   );
