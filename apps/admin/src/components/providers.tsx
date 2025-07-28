ecedb75e57692c6555855ad6ee420e33d0c7bd7c
SongMinQQ <koe7324@naver.com>
2025-07-28 17:17:03 +0900

    [FEAT] UBLE-112 다크모드 강제 비활성화 (#122)
    * UBLE-112 feat: 라이트모드 강제 설정
    
    * UBLE-112 feat: 라이트모드 강제 설정
    
    * UBLE-112 style: 주석 삭제

--- apps/admin/src/components/providers.tsx
+++ apps/admin/src/components/providers.tsx
@@ -7,10 +7,9 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
-      enableSystem
+      defaultTheme="light"
+      enableSystem={false}
       disableTransitionOnChange
-      enableColorScheme
     >
       {children}
     </NextThemesProvider>
