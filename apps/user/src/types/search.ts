659edb9510eec2cadd5c025a11b68e8d961299e0
alotofhee <ekgmljeong@gmail.com>
2025-07-28 11:41:53 +0900

    UBLE-88 feat: mapsuggestion 타입 파일 추가

--- apps/user/src/types/search.ts
+++ apps/user/src/types/search.ts
@@ -23,3 +23,22 @@ export interface BrandSuggestionResult {
 export interface BrandSuggestionResponse extends responseStatus {
   data: BrandSuggestionResult;
 }
+
+export interface MapSuggestion {
+  suggestion: string;
+  category: string;
+  id: number;
+  type: "CATEGORY" | "STORE" | "BRAND";
+  // 매장일 경우에만 있음
+  longitude?: number;
+  latitude?: number;
+  address?: string;
+}
+
+export interface MapSuggestionResult {
+  suggestionList: MapSuggestion[];
+}
+
+export interface MapSuggestionResponse extends responseStatus {
+  data: MapSuggestionResult;
+}
