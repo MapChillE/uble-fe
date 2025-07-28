aab008fa5aabd2915cdcdfc7708382f5eef21465
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-07-28 22:09:49 +0900

    UBLE-115 feat: hotjar script 추가

--- apps/user/src/app/layout.tsx
+++ apps/user/src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@workspace/ui/globals.css";
 import { Providers } from "@/components/providers";
 import { Toaster } from "sonner";
 import ServiceWorkerRegister from "./ServiceWorkerRegister";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -17,6 +18,18 @@ export default function RootLayout({
         <meta name="color-scheme" content="light" />
         <title>Uble</title>
         <link rel="icon" href="/icons/favicon.ico" sizes="32x32" />
+        <Script id="hotjar-init" strategy="afterInteractive">
+          {`
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:6477133,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `}
+        </Script>
       </head>
       <body>
         <Providers>
