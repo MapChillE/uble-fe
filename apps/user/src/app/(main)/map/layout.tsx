cdd978cb793eb8c51e15a50c8a4a41dbe0e9b67c
Dahee Jeong <55499429+alotofhee@users.noreply.github.com>
2025-07-28 11:27:51 +0900

    UBLE-102 fix: client side rendering 오류로 인한 layout 수정
    - naver is not defined 오류

--- apps/user/src/app/(main)/map/layout.tsx
+++ apps/user/src/app/(main)/map/layout.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Script from "next/script";
 
 export default function MapLayout({ children }: { children: React.ReactNode }) {
@@ -6,10 +7,14 @@ export default function MapLayout({ children }: { children: React.ReactNode }) {
       {/* 네이버 지도 및 클러스터링 스크립트 */}
       <Script
         strategy="afterInteractive"
-        type="text/javascript"
         src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=${process.env.NEXT_PUBLIC_MAP_CLIENT_ID}&submodules=geocoder`}
+        onLoad={() => {
+          const script = document.createElement("script");
+          script.src = "/MarkerClustering.js";
+          script.async = true;
+          document.body.appendChild(script);
+        }}
       />
-      <Script strategy="afterInteractive" src="/MarkerClustering.js" />
 
       <main
         style={{
