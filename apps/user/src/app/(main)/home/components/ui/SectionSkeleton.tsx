50e378b301c8df67908c6c2a8e9e715a07e5807a
SongMinQQ <koe7324@naver.com>
2025-07-29 16:20:10 +0900

    [FIX] UBLE-121 모바일 환경에서 스켈레톤 overflow시 스크롤 가능하도록 수정 (#133)
    * UBLE-121 fix: 모바일 환경에서 스켈레톤 overflow시 스크롤 가능하도록 수정
    
    * UBLE-121 fix: 스크롤 영역 수정
    
    * UBLE-121 fix: 자동 리다이렉션 rollback

--- apps/user/src/app/(main)/home/components/ui/SectionSkeleton.tsx
+++ apps/user/src/app/(main)/home/components/ui/SectionSkeleton.tsx
@@ -5,10 +5,12 @@ const SectionSkeleton = () => {
   return (
     <div className="space-y-4">
       <SectionHeader title="추천 제휴처 불러오는 중..." />
-      <div className="flex gap-4 px-4 pb-2">
-        {[1, 2, 3].map((i) => (
-          <Skeleton key={i} className="h-64 w-56 flex-shrink-0 rounded-xl" />
-        ))}
+      <div className="overflow-x-auto">
+        <div className="flex gap-4 px-4 pb-2">
+          {[1, 2, 3].map((i) => (
+            <Skeleton key={i} className="h-64 w-56 flex-shrink-0 rounded-xl" />
+          ))}
+        </div>
       </div>
     </div>
   );
