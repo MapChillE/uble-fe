dd661e3f451ddca1f4a6300657a34a1fa6a3444a
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-07-29 21:42:24 +0900

    UBLE-127 feat: VIP만 사용 가능한지 여부 판별 변수 추가

--- apps/user/src/store/useBenefitConfirmModalStore.ts
+++ apps/user/src/store/useBenefitConfirmModalStore.ts
@@ -4,10 +4,11 @@ interface BenefitConfirmModalState {
   isOpen: boolean;
   storeId: number | null;
   isVIPcock: boolean;
+  vipOnly: boolean;
   onSuccess: (() => void) | null;
   open: () => void;
   close: () => void;
-  setInfo: (storeId: number, isVIPcock: boolean) => void;
+  setInfo: (storeId: number, isVIPcock: boolean, vipOnly: boolean) => void;
   resetInfo: () => void;
   setOnSuccess: (cb: (() => void) | null) => void;
 }
@@ -16,10 +17,11 @@ const useBenefitConfirmModalStore = create<BenefitConfirmModalState>((set) => ({
   isOpen: false,
   storeId: null,
   isVIPcock: false,
+  vipOnly: false,
   onSuccess: null,
   open: () => set({ isOpen: true }),
   close: () => set({ isOpen: false }),
-  setInfo: (storeId, isVIPcock) => set({ storeId, isVIPcock }),
+  setInfo: (storeId, isVIPcock, vipOnly) => set({ storeId, isVIPcock, vipOnly }),
   resetInfo: () => set({ storeId: null, isVIPcock: false, onSuccess: null }),
   setOnSuccess: (cb) => set({ onSuccess: cb }),
 }));
