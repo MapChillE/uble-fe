0966d4be95c49ca5bca11367ccb4fa318258a5fa
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-07-31 13:18:19 +0900

    UBLE-136 comment: 불필요한 주석 삭제

--- apps/admin/src/types/statistics.ts
+++ apps/admin/src/types/statistics.ts
@@ -7,11 +7,3 @@ export interface StatisticsFilter {
 }
 
 export type AgeRangeType = 10 | 20 | 30 | 40 | 50 | 60 | 70 | 80 | 90;
-
-// export interface StatisticsFilterOption {
-//   rankTarget: Array<"BRAND" | "CATEGORY">;
-//   gender: Array<"MALE" | "FEMALE">;
-//   ageRange: Array<10 | 20 | 30 | 40 | 50 | 60 | 70 | 80 | 90 | 100>;
-//   rank: Array<"NORMAL" | "PREMIUM" | "VIP" | "VVIP" | "NONE">;
-//   benefitType: Array<"VIP" | "LOCAL" | "NORMAL">;
-// }
