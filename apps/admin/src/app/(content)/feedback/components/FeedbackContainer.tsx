e1087204b11dc38314920f19a1c763adb5029eae
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-08-02 21:14:07 +0900

    UBLE-153 feat: 피드백 열람 구현

--- apps/admin/src/app/(content)/feedback/components/FeedbackContainer.tsx
+++ apps/admin/src/app/(content)/feedback/components/FeedbackContainer.tsx
@@ -1,51 +1,36 @@
 "use client";
 import { Fragment, useState } from "react";
 import FeedbackStatistics from "./FeedbackStatistics";
-import { FeedbackData } from "@/types/feedback";
 import FeedbackList from "./FeedbackList";
 import Pagination from "./Pagination";
-
-const PAGE_SIZE = 10;
+import { useQuery } from "@tanstack/react-query";
+import { fetchFeedbackList } from "@/service/feedback";
+import FeedbackSkeleton from "./ui/FeedbackSkeleton";
+import FeedbackError from "./ui/FeedbackError";
 
 const FeedbackContainer = () => {
   const [currentPage, setCurrentPage] = useState(1);
 
   /** 향후 구현 fetching 함수 */
-  // const { data, isLoading, isError } = useQuery({
-  //   queryKey: ["feedbacks", currentPage],
-  //   queryFn: () => fetchFeedbacks(currentPage),
-  //   keepPreviousData: true,
-  // });
-  const feedbackData: FeedbackData = {
-    content: [
-      {
-        title: "UI 개선 요청",
-        content: "지도에서 핀을 더 크게 해주세요.",
-        score: 4,
-        createdAt: "2025-07-31T05:20:49.219Z",
-        nickname: "오잉",
-      },
-      {
-        title: "서비스가 정말 좋아요!",
-        content:
-          "지도에서 제휴처를 쉽게 찾을 수 있어서 너무 편리합니다. 할인 혜택도 잘 적용되고 있어요.",
-        score: 5,
-        createdAt: "2025-07-30T14:30:22.123Z",
-        nickname: "김민수",
-      },
-    ],
-    totalCount: 30,
-    totalPages: 3,
-  };
+  const { data, isLoading, isError, refetch } = useQuery({
+    queryKey: ["feedbacks", currentPage],
+    queryFn: () => fetchFeedbackList(currentPage - 1),
+  });
+
+  if (isLoading) {
+    return <FeedbackSkeleton />;
+  }
+
+  if (isError || !data) {
+    return <FeedbackError refetch={refetch} />;
+  }
+  console.log(data.data.totalCount);
+  const { content, totalCount, totalPages } = data.data;
   return (
     <Fragment>
-      <FeedbackStatistics feedbackData={feedbackData} />
-      <FeedbackList feedbacks={feedbackData.content} />
-      <Pagination
-        currentPage={currentPage}
-        totalPages={feedbackData.totalPages}
-        onPageChange={setCurrentPage}
-      />
+      <FeedbackStatistics totalCount={totalCount} />
+      <FeedbackList feedbacks={content} />
+      <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage} />
     </Fragment>
   );
 };
