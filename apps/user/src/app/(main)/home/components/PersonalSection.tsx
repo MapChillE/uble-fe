2c517d8cfdef69247380caa7fea7babce9c6c7b8
alotofhee <ekgmljeong@gmail.com>
2025-08-03 17:13:01 +0900

    UBLE-151 style: 캐릭터 삽입

--- apps/user/src/app/(main)/home/components/PersonalSection.tsx
+++ apps/user/src/app/(main)/home/components/PersonalSection.tsx
@@ -10,6 +10,7 @@ import { useRouter } from "next/navigation";
 import SectionSkeleton from "./ui/SectionSkeleton";
 import SectionError from "./ui/SectionError";
 import EmptyRecommend from "./ui/EmptyRecommend";
+import Image from "next/image";
 
 const PersonalSection = () => {
   const nickname = useUserStore((state) => state?.user?.nickname);
@@ -45,36 +46,43 @@ const PersonalSection = () => {
   //   console.log(recommendationsList);
   // }, [recommendationsList]);
   return (
-    <div className="space-y-4">
+    <div className="relative pt-4">
       <SectionHeader
         title={`${nickname ? nickname : "회원"}님을 위한 혜택`}
         isScroll
         Scrollref={scrollContainerRef}
       />
-      <div
-        className="scrollbar-hide flex gap-4 overflow-x-auto px-4 pb-2"
-        style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
-        ref={scrollContainerRef}
-      >
-        {recommendationsList.length === 0 ? (
-          <EmptyRecommend>
-            아직 추천할 만한 제휴처가 없습니다.
-            <br />
-            서비스를 이용하시고 제휴처 추천을 받아보세요!
-          </EmptyRecommend>
-        ) : (
-          recommendationsList.map((item) => (
-            <DynamicCard
-              data={item}
-              key={item.brandId}
-              onClick={() =>
-                router.push(
-                  `/map?lat=${item.latitude}&lng=${item.longitude}&storeId=${item.storeId}`
-                )
-              }
-            />
-          ))
-        )}
+      <div className="left-55 absolute -top-3 z-10 -translate-x-1/2">
+        <Image src="/assets/uble_character_hi.png" alt="Uble character" width={80} height={80} />
+      </div>
+
+      <div className="space-y-4 p-4">
+        {/*  rounded-xl bg-gradient-to-br from-green-100 via-emerald-100 to-teal-100 shadow-sm*/}
+        <div
+          className="scrollbar-hide flex gap-4 overflow-x-auto pb-2"
+          style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
+          ref={scrollContainerRef}
+        >
+          {recommendationsList.length === 0 ? (
+            <EmptyRecommend>
+              아직 추천할 만한 제휴처가 없습니다.
+              <br />
+              서비스를 이용하시고 제휴처 추천을 받아보세요!
+            </EmptyRecommend>
+          ) : (
+            recommendationsList.map((item) => (
+              <DynamicCard
+                data={item}
+                key={item.brandId}
+                onClick={() =>
+                  router.push(
+                    `/map?lat=${item.latitude}&lng=${item.longitude}&storeId=${item.storeId}`
+                  )
+                }
+              />
+            ))
+          )}
+        </div>
       </div>
     </div>
   );
