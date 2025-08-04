a14f6ddf0e476b59a7c73845a8298fd219eb3b40
SongMinQQ <koe7324@naver.com>
2025-08-04 22:18:59 +0900

    [FEAT] UBLE-166 사이트 소개 페이지 추가 (#204)
    * UBLE-166 docs: intro 페이지에서 사용하는 이미지 소스 추가
    
    * UBLE-166 feat: UBLE intro 페이지 구현
    
    * UBLE-166 feat: 서비스를 처음 이용하는 사용자에게 intro 페이지로 유도하는 안내 문구 추가

--- apps/user/src/app/(no-layout)/page.tsx
+++ apps/user/src/app/(no-layout)/page.tsx
@@ -3,6 +3,8 @@
 import Image from "next/image";
 import { motion, Variants } from "framer-motion";
 import KakaoLoginBtn from "./components/KakaoLoginBtn";
+import { ArrowRight } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const taglineVariants: Variants = {
   hidden: {},
@@ -28,6 +30,7 @@ const lineVariants: Variants = {
 };
 
 export default function Page() {
+  const router = useRouter();
   return (
     <div className="from-action-green/10 to-action-green/10 relative flex h-[100dvh] flex-col items-center justify-between overflow-hidden overscroll-none bg-gradient-to-b via-slate-50">
       <motion.span
@@ -83,7 +86,20 @@ export default function Page() {
           </motion.p>
         </motion.div>
       </section>
-
+      <motion.div
+        onClick={() => router.push("/intro")}
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.5, duration: 0.8 }}
+        className="mb-2 mt-6 flex cursor-pointer flex-col items-center text-center"
+      >
+        <div className="bg-action-green/10 hover:bg-action-green/20 flex items-center space-x-2 rounded-full px-4 py-2 transition">
+          <span className="text-action-green font-semibold">
+            처음이세요? UBLE 서비스 소개부터 볼게요
+          </span>
+          <ArrowRight className="text-action-green h-5 w-5" />
+        </div>
+      </motion.div>
       <motion.div
         className="flex-shrink-0 px-6"
         style={{
