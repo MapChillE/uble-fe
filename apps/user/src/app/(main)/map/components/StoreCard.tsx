784df0f360cfd6e10980fa6a33d2889eeadce529
Dahee Jeong <55499429+alotofhee@users.noreply.github.com>
2025-08-06 07:37:13 +0900

    UBLE-176 fix: conflict 해결

--- apps/user/src/app/(main)/map/components/StoreCard.tsx
+++ apps/user/src/app/(main)/map/components/StoreCard.tsx
@@ -98,11 +98,7 @@ export const DetailCard = ({ data }: { data: StoreDetail }) => (
               <div className="space-y-2 pt-4">
                 <h3 className="text-lg font-semibold">멤버십 바코드</h3>
               </div>
-              <BarcodeContainer
-                storeId={data.storeId}
-                isVIPcock={benefit.type === "VIP"}
-                vipOnly={data.benefitList[0]?.minRank === "VIP"}
-              />
+              <BarcodeContainer storeDetail={data} />
             </Fragment>
           )}
         </div>
