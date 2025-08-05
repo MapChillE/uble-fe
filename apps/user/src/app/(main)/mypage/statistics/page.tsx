7ecf0ab458d39d76126c018481cad2478bcef319
alotofhee <ekgmljeong@gmail.com>
2025-08-06 07:27:54 +0900

    UBLE-179 feat: 상세 통계 페이지 생성

---
+++ apps/user/src/app/(main)/mypage/statistics/page.tsx
@@ -0,0 +1,11 @@
+// import StatisticsDetail from "./components/StatisticsDetail";
+
+const StatisticsPage = () => {
+  return (
+    <div className="relative w-full bg-white pb-20">
+      <div className="space-y-6 px-4 py-6">{/* <StatisticsDetail /> */}</div>
+    </div>
+  );
+};
+
+export default StatisticsPage;
